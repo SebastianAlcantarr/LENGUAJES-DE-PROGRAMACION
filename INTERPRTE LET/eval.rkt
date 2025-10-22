3347d270198f7ec8a4e75b02f7d77fab2d8781e2
SebastianAlcantarr <sebastianalcantar323@gmail.com>
2025-10-22 11:04:41 -0700

    Add files via upload

---
+++ INTERPRTE LET/eval.rkt
@@ -0,0 +1,105 @@
+#lang racket
+(require "ast.rkt")
+(require "env.rkt")
+(require "vals.rkt")
+
+
+(define (value-of expr env)
+  (cond ((const-exp? expr)
+         (num-val(const-exp-num expr)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        
+        ((zero-exp?  expr)
+         (bool-val (zero? (expval->num (value-of (zero-exp-exp1 expr)env)))))
+        
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+        
+        ((if-exp? expr)
+         (let((val1 (value-of (if-exp-exp1 expr) env )))
+           (if (eqv?(expval->bool val1)#t)
+               (value-of (if-exp-exp2 expr) env)
+               (value-of (if-exp-exp3 expr) env ))))
+
+        
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+        
+        ((diff-exp? expr)
+         (let ((val1 (value-of (diff-exp-exp1 expr)env))
+               (val2 (value-of (diff-exp-exp2 expr)env)))
+           (num-val(-(expval->num val1)
+                     (expval->num val2 )))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        
+        ((var-exp? expr)
+         (apply-env env ( var-exp-var expr)))
+        
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        
+        ((let-exp? expr)
+         (let ((val1 (value-of (let-exp-exp1 expr) env)))
+           (value-of (let-exp-body expr) (extend-env (let-exp-var expr) val1 env))))
+
+;;;;;;;;;;;;;;;;;; --------- LISTAS -------------------------------;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        
+        ((cons-exp? expr)
+         (let ((val1 (value-of (cons-exp-car expr) env))
+               (val2 (value-of (cons-exp-cdr expr) env)))
+           (pair-val val1 val2)))
+
+        ((empty-exp? expr)
+         (empty-val))
+
+
+        ((car-exp? expr)
+         (let ((val (value-of (car-exp-exp1 expr) env)))
+           (let ([pair (expval->pair val)])
+             (pair-val-car pair))))
+
+
+        ((cdr-exp? expr)
+         (let ((val (value-of (cdr-exp-exp1 expr)env)))
+           (let ((pair (expval->pair val)))
+             (pair-val-cdr pair))))
+
+        ((null-exp? expr )
+         (let ((val (value-of (null-exp-exp1 expr )env)))
+           (expval->empty val)
+        ))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(define (init-env)
+  (extend-env 'imperio-romano (bool-val #f)(extend-env'satan (num-val 666)
+                                                      (empty-env))))
+
+
+(define (value-of-program prgm)
+  (match prgm
+    ((a-program expr)
+     (value-of expr (init-env)))
+    (else
+     (error "no programa "))))
+
+
+
+(define e1 (const-exp 10))
+(define e2 (const-exp 90))
+(define e3 (const-exp 90))
+(define e4 (const-exp 40))
+(define lista (cons-exp e1 (cons-exp e2 (empty-exp))))
+(define resta (diff-exp e3 e4 ))
+
+
+
+(provide
+ (contract-out
+  (value-of-program (-> let-program? expval?))
+  (value-of (-> let-expression?  environment? expval?))))
+
+
+ 
